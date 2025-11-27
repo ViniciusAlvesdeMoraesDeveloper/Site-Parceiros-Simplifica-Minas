89de3ce3ad4b60d73041c93db284de9d9dbfc6d4
Vinícius Alves <vinialvesmoraes18@gmail.com>
2025-11-27 15:22:38 -0300

    Atualização de informações

--- components/header/header_logo/header_logo_container/index.tsx
+++ components/header/header_logo/header_logo_container/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export function HeaderLogoContainer({ children }: { children: ReactNode }) {
     return (
-        <div className=" md:w-2/10 ml-10">
+        <div className=" md:w-2/10 ml-10 mt-5">
             {children}
         </div>
 
--- components/header/header_logo/header_logo_image/index.tsx
+++ components/header/header_logo/header_logo_image/index.tsx
@@ -7,14 +7,14 @@ interface HeaderLogoImageProps {
 }
 
 export function HeaderLogoImage({
-    mobileSize = "h-18",  
+    mobileSize = "h-22",  
     desktopSize = "h-24", 
 }: HeaderLogoImageProps) {
     return (
         <Image
             src="/header_logo.png"
             alt="Logo Simplifica Minas"
-            className={`${mobileSize} md:${desktopSize} w-auto object-contain transition-all duration-300`}
+            className={`${mobileSize} md:${desktopSize} w-90 object-contain transition-all duration-300 mb-2`}
             width={180}
             height={80}
             priority
